10bd3a74f9bb7dccb45f7a6bc57a503127ecc850
“Mdmitriy119E” <dumitru.manea@endava.com>
2023-04-15 11:27:33 +0300

    feat: project init

---
+++ iRecipe/iRecipe/iRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRecipeApp.swift
+//  iRecipe
+//
+//  Created by Dumitru Manea on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct iRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
